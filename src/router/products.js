bb998760acfb5a4c9a01708755e6585f45db6881
Eduardo Degan <edegan@furb.br>
2023-08-24 19:23:31 -0300

    products api to bd

--- src/router/products.js
+++ src/router/products.js
@@ -3,8 +3,23 @@ const router = express.Router();
 const mysql = require("../mysql").pool;
 
 router.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "GET products: all good",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error,
+        response: null,
+      });
+    }
+    conn.query("SELECT * FROM products;", (error, result, fields) => {
+      conn.release();
+      if (error) {
+        return res.status(500).send({
+          error: error,
+          response: null,
+        });
+      }
+      return res.status(200).send({ response: result });
+    });
   });
 });
 
@@ -40,30 +55,90 @@ router.post("/", (req, res, next) => {
 });
 
 router.get("/:_id", (req, res, next) => {
-  const id = req.params.id_product;
-
-  if (id === "special") {
-    res.status(200).send({
-      message: "GET special product: all good",
-      id: id,
-    });
-  } else {
-    res.status(200).send({
-      message: "GET especific product: all good",
-      id: id,
-    });
-  }
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error,
+        response: null,
+      });
+    }
+    conn.query(
+      "SELECT * FROM products WHERE id_product = ?;",
+      [req.params.id_product],
+      (error, result, fields) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+        return res.status(200).send({ response: result });
+      }
+    );
+  });
 });
 
 router.patch("/", (req, res, next) => {
-  res.status(201).send({
-    message: "PATCH product: all good",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error,
+        response: null,
+      });
+    }
+
+    conn.query(
+      `UPDATE products
+          SET name        = ?,
+              price       = ?
+        WHERE id_product  = ?`,
+      [req.body.name, req.body.price, req.body.id_product],
+      (error, result, fields) => {
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+
+        res.status(202).send({
+          message: "Product updated successfully",
+        });
+      }
+    );
   });
 });
 
 router.delete("/", (req, res, next) => {
-  res.status(201).send({
-    message: "DELETE product: all good",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error,
+        response: null,
+      });
+    }
+
+    conn.query(
+      `DELETE FROM products WHERE id_product = ?`,
+      [req.body.id_product],
+      (error, result, fields) => {
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error: error,
+            response: null,
+          });
+        }
+
+        res.status(202).send({
+          message: "Product removed successfully",
+        });
+      }
+    );
   });
 });
 
